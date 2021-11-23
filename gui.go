879c2703f0a094dd26d5c5339c1b5a336c906b9d
Jerome Amon <aaboujerome@gmail.com>
2021-11-23 23:00:04 +0100

    feat: add multiple features like navigation inside the maze with arrow keys

--- gui.go
+++ gui.go
@@ -25,19 +25,28 @@ const (
 	HELP     = "help"
 	POSITION = "position"
 	TIMER    = "timer"
+	STATUS   = "status"
 
 	TWIDTH = 11
 	PWIDTH = 30
+	SWIDTH = 45
 )
 
 var (
 	// default maze size.
-	MAZEHEIGHT int = 20
-	MAZEWIDTH  int = 25
+	MAZEHEIGHT int = 10
+	MAZEWIDTH  int = 15
 
 	// control timer in updateTimerView.
 	stopTimer  = make(chan struct{})
 	resetTimer = make(chan struct{})
+	// control game status. 1 means paused.
+	// 0 means ready to play, 2 means empty.
+	// 3 means error so need to restart game.
+	pauseGame    = make(chan uint8, 3)
+	isGamePaused = false
+
+	cursorPosition = make(chan string, 10)
 
 	// control goroutines.
 	exit = make(chan struct{})
@@ -116,7 +125,7 @@ func main() {
 		return
 	}
 	timerView.Title = " Timer "
-	timerView.FgColor = gocui.ColorYellow
+	timerView.FgColor = gocui.ColorGreen
 	timerView.SelBgColor = gocui.ColorBlack
 	timerView.SelFgColor = gocui.ColorYellow
 	timerView.Editable = false
@@ -135,21 +144,33 @@ func main() {
 	positionView.SelFgColor = gocui.ColorYellow
 	positionView.Editable = false
 	positionView.Wrap = false
-	fmt.Fprint(positionView, "X : 0 | Y : 0")
+
+	// Status view.
+	statusView, err := g.SetView(STATUS, PWIDTH+1, maxY-3, SWIDTH, maxY-1)
+	if err != nil && err != gocui.ErrUnknownView {
+		log.Println("Failed to create status view:", err)
+		return
+	}
+	statusView.Title = " Status "
+	statusView.FgColor = gocui.ColorRed
+	statusView.SelBgColor = gocui.ColorBlack
+	statusView.SelFgColor = gocui.ColorRed
+	statusView.Editable = false
+	statusView.Wrap = false
 
 	// Help view.
-	helpView, err := g.SetView(HELP, PWIDTH+1, maxY-3, maxX-1, maxY-1)
+	helpView, err := g.SetView(HELP, SWIDTH+1, maxY-3, maxX-1, maxY-1)
 	if err != nil && err != gocui.ErrUnknownView {
 		log.Println("Failed to create help view:", err)
 		return
 	}
 	helpView.Title = " Instructions "
-	helpView.FgColor = gocui.ColorYellow
+	helpView.FgColor = gocui.ColorWhite
 	helpView.SelBgColor = gocui.ColorBlack
 	helpView.SelFgColor = gocui.ColorYellow
 	helpView.Editable = false
 	helpView.Wrap = false
-	fmt.Fprint(helpView, "CTRL+N [Generate new maze] - CTRL+R [Reset position] - CTRL+Q [Close Maze]")
+	fmt.Fprint(helpView, "CTRL+N [New Maze] - CTRL+R [Reset Game] - CTRL+P [Pause/Resume] - CTRL+Q [Close Maze]")
 
 	// Apply keybindings to program.
 	if err = keybindings(g); err != nil {
@@ -164,6 +185,12 @@ func main() {
 
 	wg.Add(1)
 	go updateTimerView(g)
+
+	wg.Add(1)
+	go updatePositionView(g)
+
+	wg.Add(1)
+	go updateStatusView(g)
 
 	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
 		// close(exit)
@@ -197,8 +224,15 @@ func layout(g *gocui.Gui) error {
 		return err
 	}
 
+	// Status view.
+	_, err = g.SetView(STATUS, PWIDTH+1, maxY-3, SWIDTH, maxY-1)
+	if err != nil && err != gocui.ErrUnknownView {
+		log.Println("Failed to create status view:", err)
+		return err
+	}
+
 	// Help view.
-	_, err = g.SetView(HELP, PWIDTH+1, maxY-3, maxX-1, maxY-1)
+	_, err = g.SetView(HELP, SWIDTH+1, maxY-3, maxX-1, maxY-1)
 	if err != nil && err != gocui.ErrUnknownView {
 		log.Println("Failed to create help view:", err)
 		return err
@@ -318,6 +352,74 @@ func updateTimerView(g *gocui.Gui) {
 	}
 }
 
+// updatePositionView displays current cursor coordinates.
+func updatePositionView(g *gocui.Gui) {
+	defer wg.Done()
+	var pos string
+	positionView, err := g.View(POSITION)
+	if err != nil {
+		log.Println("Failed to get position view for updating:", err)
+		return
+	}
+
+	for {
+
+		select {
+
+		case <-exit:
+			return
+
+		case pos = <-cursorPosition:
+
+			g.Update(func(g *gocui.Gui) error {
+				positionView.Clear()
+				fmt.Fprintf(positionView, pos)
+				return nil
+			})
+		}
+
+		time.Sleep(10 * time.Millisecond)
+	}
+}
+
+// updateStatusView displays current game status.
+func updateStatusView(g *gocui.Gui) {
+	defer wg.Done()
+	var pval uint8
+
+	statusView, err := g.View(STATUS)
+	if err != nil {
+		log.Println("Failed to get status view for updating:", err)
+		return
+	}
+
+	for {
+
+		select {
+
+		case <-exit:
+			return
+
+		case pval = <-pauseGame:
+
+			g.Update(func(g *gocui.Gui) error {
+				statusView.Clear()
+				if pval == 1 {
+					fmt.Fprintf(statusView, ":: PAUSE")
+				} else if pval == 0 {
+					fmt.Fprintf(statusView, ":: READY")
+				} else if pval == 3 {
+					fmt.Fprintf(statusView, ":: ERROR")
+				}
+
+				return nil
+			})
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+}
+
 // createMazeView displays a temporary box to contain the new generated maze.
 func createMazeView(g *gocui.Gui, v *gocui.View, data strings.Builder) error {
 
@@ -352,14 +454,8 @@ func createMazeView(g *gocui.Gui, v *gocui.View, data strings.Builder) error {
 
 	// g.Cursor = true
 
-	// bind Ctrl+Q and Escape keys to close the input box.
-	if err = g.SetKeybinding(name, gocui.KeyCtrlQ, gocui.ModNone, closeMazeView); err != nil {
-		log.Println(err)
-		return err
-	}
-
-	if err = g.SetKeybinding(name, gocui.KeyEsc, gocui.ModNone, closeMazeView); err != nil {
-		log.Println(err)
+	if err = mazeKeybindings(g, name); err != nil {
+		log.Println("Failed to bind keys to maze view:", err)
 		return err
 	}
 
@@ -369,13 +465,62 @@ func createMazeView(g *gocui.Gui, v *gocui.View, data strings.Builder) error {
 
 	// move cursor to maze entrance.
 	x, _ := mazeView.Size()
-	g.Cursor = false
 	if err = mazeView.SetCursor(x/2+1, 0); err != nil {
 		log.Println("Failed to set cursor at middle of maze view:", err)
 	}
+
 	// display the rat at the entrance.
-	mazeView.EditWrite('♣')
+	g.Cursor = true
+	v.EditWrite('♣')
 	v.Frame = false
+
+	// update status and position.
+	isGamePaused = false
+	pauseGame <- 0
+	cx, cy := v.Cursor()
+	cursorPosition <- fmt.Sprintf("(X:%d | Y:%d)", cx, cy)
+	return nil
+}
+
+// mazeKeybindings binds multiple keys to maze view.
+func mazeKeybindings(g *gocui.Gui, name string) error {
+	var err error
+
+	if err = g.SetKeybinding(name, gocui.KeyCtrlQ, gocui.ModNone, closeMazeView); err != nil {
+		return err
+	}
+
+	if err = g.SetKeybinding(name, gocui.KeyEsc, gocui.ModNone, closeMazeView); err != nil {
+		return err
+	}
+
+	if err = g.SetKeybinding(name, gocui.KeyCtrlP, gocui.ModNone, pauseResumeGame); err != nil {
+		return err
+	}
+
+	if err = g.SetKeybinding(name, gocui.KeySpace, gocui.ModNone, pauseResumeGame); err != nil {
+		return err
+	}
+
+	if err = g.SetKeybinding(name, gocui.KeyCtrlR, gocui.ModNone, resetGame); err != nil {
+		return err
+	}
+
+	if err = g.SetKeybinding(name, gocui.KeyArrowUp, gocui.ModNone, moveUp); err != nil {
+		return err
+	}
+
+	if err = g.SetKeybinding(name, gocui.KeyArrowDown, gocui.ModNone, moveDown); err != nil {
+		return err
+	}
+
+	if err = g.SetKeybinding(name, gocui.KeyArrowLeft, gocui.ModNone, moveLeft); err != nil {
+		return err
+	}
+
+	if err = g.SetKeybinding(name, gocui.KeyArrowRight, gocui.ModNone, moveRight); err != nil {
+		return err
+	}
 
 	return nil
 }
@@ -395,8 +540,10 @@ func closeMazeView(g *gocui.Gui, mv *gocui.View) error {
 		return err
 	}
 
-	// stop timer.
+	// stop timer and update game status.
 	stopTimer <- struct{}{}
+	isGamePaused = false
+	pauseGame <- 2
 
 	return nil
 }
@@ -445,9 +592,16 @@ func nextView(g *gocui.Gui, v *gocui.View) error {
 		}
 
 	case POSITION:
+		// move the focus on Status view.
+		if _, err := g.SetCurrentView(STATUS); err != nil {
+			log.Println("Failed to set focus on status view:", err)
+			return err
+		}
+
+	case STATUS:
 		// move the focus on Help view.
 		if _, err := g.SetCurrentView(HELP); err != nil {
-			log.Println("Failed to set focus on stats view:", err)
+			log.Println("Failed to set focus on help view:", err)
 			return err
 		}
 
@@ -457,6 +611,231 @@ func nextView(g *gocui.Gui, v *gocui.View) error {
 			log.Println("Failed to set focus on maze view:", err)
 			return err
 		}
+	}
+
+	return nil
+}
+
+// pauseResumeGame stops/resumes the timer and disable/enable navigation and reset keys.
+func pauseResumeGame(g *gocui.Gui, mv *gocui.View) error {
+	var err error
+
+	stopTimer <- struct{}{}
+
+	// inverse the game status.
+	isGamePaused = !isGamePaused
+
+	if isGamePaused {
+		pauseGame <- 1
+		g.Cursor = false
+		// game paused so disable controls keys bindings.
+		for _, key := range []gocui.Key{gocui.KeyCtrlR, gocui.KeyArrowUp, gocui.KeyArrowDown, gocui.KeyArrowLeft, gocui.KeyArrowRight} {
+			if err = g.DeleteKeybinding(mv.Name(), key, gocui.ModNone); err != nil {
+				log.Printf("Failed to pause the game. error disabling key %v on maze view: %v", key, err)
+				return err
+			}
+		}
+
+		return nil
+	}
+
+	pauseGame <- 0
+	g.Cursor = true
+	// game resumed so enable controls keys bindings.
+	if err = g.SetKeybinding(mv.Name(), gocui.KeyCtrlR, gocui.ModNone, resetGame); err != nil {
+		log.Println("Failed to resume the game. error enabling keys on maze view:", err)
+		return err
+	}
+
+	if err = g.SetKeybinding(mv.Name(), gocui.KeyArrowDown, gocui.ModNone, moveDown); err != nil {
+		log.Println("Failed to resume the game. error enabling keys on maze view:", err)
+		return err
+	}
+
+	if err = g.SetKeybinding(mv.Name(), gocui.KeyArrowUp, gocui.ModNone, moveUp); err != nil {
+		log.Println("Failed to resume the game. error enabling keys on maze view:", err)
+		return err
+	}
+
+	if err = g.SetKeybinding(mv.Name(), gocui.KeyArrowRight, gocui.ModNone, moveRight); err != nil {
+		log.Println("Failed to resume the game. error enabling keys on maze view:", err)
+		return err
+	}
+
+	if err = g.SetKeybinding(mv.Name(), gocui.KeyArrowLeft, gocui.ModNone, moveLeft); err != nil {
+		log.Println("Failed to resume the game. error enabling keys on maze view:", err)
+		return err
+	}
+
+	return nil
+}
+
+// resetGame reinitialize the timer and move to entrance position.
+func resetGame(g *gocui.Gui, mv *gocui.View) error {
+	resetTimer <- struct{}{}
+	pauseGame <- 0
+	x, _ := mv.Size()
+	g.Cursor = true
+	if err := mv.SetCursor(x/2+1, 0); err != nil {
+		log.Println("Failed to set cursor at middle of maze view:", err)
+		return err
+	}
+
+	mv.EditWrite('♣')
+	cx, cy := mv.Cursor()
+	cursorPosition <- fmt.Sprintf("(X:%d | Y:%d)", cx, cy)
+	return nil
+}
+
+// noWallBelow returns true if there is only space at position (x,y+1).
+func noWallBelow(v *gocui.View) bool {
+	cx, cy := v.Cursor()
+
+	// check for underscore-based south wall at current position.
+	// if there is any error, we notify user to quit the program.
+	l, err := v.Line(cy)
+	if err != nil {
+		log.Printf("Failed to check maze bottom direction (%d,%d). err: %v", cx, cy, err)
+		pauseGame <- 3
+		return false
+	}
+
+	if l[cx] == '_' {
+		return false
+	}
+
+	// check if we are still into the grid.
+	if (cy + 1) > MAZEHEIGHT {
+		return false
+	}
+
+	// check for pipe-based south wall at next position.
+	l, err = v.Line(cy + 1)
+	if err != nil {
+		log.Printf("Failed to check maze bottom direction (%d,%d). err: %v", cx, cy+1, err)
+		pauseGame <- 3
+		return false
+	}
+
+	if l[cx] == '|' {
+		return false
+	}
+
+	return true
+}
+
+// moveDown moves cursor to currentX, (currentY + 1) position if there is no wall there.
+func moveDown(g *gocui.Gui, v *gocui.View) error {
+	if v != nil && noWallBelow(v) == true {
+		v.MoveCursor(0, 1, false)
+		cx, cy := v.Cursor()
+		cursorPosition <- fmt.Sprintf("(X:%d | Y:%d)", cx, cy)
+	}
+
+	return nil
+}
+
+// noWallAbove returns true if there is only space at position (x,y-1).
+func noWallAbove(v *gocui.View) bool {
+
+	// check if we are still into the grid.
+	cx, cy := v.Cursor()
+	if (cy - 1) < 0 {
+		return false
+	}
+
+	l, err := v.Line(cy - 1)
+	if err != nil {
+		log.Printf("Failed to check maze up direction (%d,%d). err: %v", cx, cy-1, err)
+		// signal/status to quit the program.
+		pauseGame <- 3
+		return false
+	}
+
+	if l[cx] == '_' || l[cx] == '|' {
+		return false
+	}
+
+	return true
+}
+
+// moveUp moves cursor to currentX, (currentY - 1) position if there is no wall there.
+func moveUp(g *gocui.Gui, v *gocui.View) error {
+	if v != nil && noWallAbove(v) == true {
+		v.MoveCursor(0, -1, false)
+		cx, cy := v.Cursor()
+		cursorPosition <- fmt.Sprintf("(X:%d | Y:%d)", cx, cy)
+	}
+
+	return nil
+}
+
+// noWallOnRight returns true if there is no wall at position (x+1,y).
+func noWallOnRight(v *gocui.View) bool {
+
+	// check if we are still into the grid.
+	cx, cy := v.Cursor()
+	if (cx + 1) > (2*MAZEWIDTH)-1 {
+		return false
+	}
+
+	l, err := v.Line(cy)
+	if err != nil {
+		log.Printf("Failed to check maze up direction (%d,%d). err: %v", cx, cy, err)
+		// signal/status to quit the program.
+		pauseGame <- 3
+		return false
+	}
+
+	if l[cx+1] == '|' {
+		return false
+	}
+
+	return true
+}
+
+// moveRight moves cursor to (currentX+1, currentY) position if there is no wall there.
+func moveRight(g *gocui.Gui, v *gocui.View) error {
+	if v != nil && noWallOnRight(v) == true {
+		// there is data to next line.
+		v.MoveCursor(1, 0, false)
+		cx, cy := v.Cursor()
+		cursorPosition <- fmt.Sprintf("(X:%d | Y:%d)", cx, cy)
+	}
+
+	return nil
+}
+
+// noWallOnLeft returns true if there is no wall at position (x-1,y).
+func noWallOnLeft(v *gocui.View) bool {
+
+	// check if we are still into the grid.
+	cx, cy := v.Cursor()
+	if (cx - 1) < 0 {
+		return false
+	}
+
+	l, err := v.Line(cy)
+	if err != nil {
+		log.Printf("Failed to check maze up direction (%d,%d). err: %v", cx, cy, err)
+		pauseGame <- 3
+		return false
+	}
+
+	if l[cx-1] == '|' {
+		return false
+	}
+
+	return true
+}
+
+// moveLeft moves cursor to (currentX-1, currentY) position if there is no wall there.
+func moveLeft(g *gocui.Gui, v *gocui.View) error {
+	if v != nil && noWallOnLeft(v) == true {
+		// there is data to next line.
+		v.MoveCursor(-1, 0, false)
+		cx, cy := v.Cursor()
+		cursorPosition <- fmt.Sprintf("(X:%d | Y:%d)", cx, cy)
 	}
 
 	return nil
